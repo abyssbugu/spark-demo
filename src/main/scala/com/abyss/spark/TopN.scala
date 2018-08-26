4498a5240845013396c94c72602395b6ccb084d8
Abyss <547161555@qq.com>
2018-08-26 18:09:02 +0800

    利用spark分析点击流日志数据进行URL的TopN排序

---
+++ src/main/scala/com/abyss/spark/TopN.scala
@@ -0,0 +1,33 @@
+package com.abyss.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Abyss on 2018/8/26.
+  * description:
+  */
+object TopN {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("UV").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val result = sc.textFile("/Users/abyss/Dev/Demos/spark-wordcount/access.log")
+      //每行进行切分
+      .map(_.split(" "))
+      //滤去丢失字段和空的url的数据
+      .filter(x => x.length > 10 && x(10) != "\"-\"")
+      //获取url计数为1
+      .map(x => (x(10), 1))
+      //统计每个url的访问次数
+      .reduceByKey(_ + _)
+      //按次数进行降序排列
+      .sortBy(_._2, false)
+      //取前5
+      .take(5)
+
+    for (x <- result) {
+      println(x._1 + "--------------" + x._2)
+    }
+    sc.stop()
+  }
+
+}
